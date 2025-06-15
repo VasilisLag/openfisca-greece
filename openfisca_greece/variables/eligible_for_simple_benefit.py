3739e2e81da6ea29805bd9a941c9d984f664eda2
VasilisLag <vlagop98@gmail.com>
2025-06-15 15:48:30 +0300

    Modify: Add simple benefit mock check

--- openfisca_greece/variables/eligible_for_simple_benefit.py
+++ openfisca_greece/variables/eligible_for_simple_benefit.py
@@ -5,7 +5,7 @@ class eligible_for_simple_benefit(Variable):
     value_type = bool
     entity = Person
     label = "Επιλεξιμότητα για απλό επίδομα"
-    definition_period = MONTH
+    definition_period = YEAR
 
     def formula(persons, period, parameters):
-        return persons("salary", period) < 12000
+        return persons("salary", period.this_year, options=[ADD]) < 12000
